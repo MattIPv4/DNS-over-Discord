6f3c10ff635db4df92f92a65c06967943579f3dd
MattIPv4 <me@mattcowley.co.uk>
2020-12-22 13:07:23 +0000

    Relying on cwd was a bad idea

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+
 const express = require('express');
 const { InteractionType, verifyKeyMiddleware } = require('discord-interactions');
 const Privacy = require('./utils/privacy');
