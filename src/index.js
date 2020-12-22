c07b97931c3782fbc8ddff88015ad788111dd746
MattIPv4 <me@mattcowley.co.uk>
2020-12-22 12:57:08 +0000

    Add .env

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { InteractionType, verifyKeyMiddleware } = require('discord-interactions');
 const Privacy = require('./utils/privacy');
