eafcb960cfe3e8dfb20190a3896e5f81104f53ba
MattIPv4 <me@mattcowley.co.uk>
2021-10-17 13:24:18 +0100

    Run webpack directly, not using wrangler defaults

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path');
-const env = require('dotenv').config({ path: path.join(__dirname, `${process.env.NODE_ENV}.env`) });
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const env = require('dotenv').config({ path: path.join(__dirname, `${NODE_ENV}.env`) });
 const { DefinePlugin } = require('webpack');
 const WorkersSentryWebpackPlugin = require('workers-sentry/webpack');
 const build = require('./src/build');
 
+console.log(`Using ${NODE_ENV} environment for build...`);
+
 module.exports = {
     target: 'webworker',
     entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'worker.js',
+    },
     plugins: [
         // Hook in the commands build process before each webpack run
         { apply: compiler => compiler.hooks.beforeRun.tapPromise('PrepareBuildBeforeWebpack', build) },
@@ -15,7 +22,7 @@ module.exports = {
         new DefinePlugin(Object.entries(env.parsed).reduce((obj, [ key, val ]) => {
             obj[`process.env.${key}`] = JSON.stringify(val);
             return obj;
-        }, { 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) })),
+        }, { 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) })),
 
         // Publish source maps to Sentry on each build
         new WorkersSentryWebpackPlugin(
