3bc1737356b9be8712e90d2242b1c484aa5d0dd2
Matt (IPv4) Cowley <me@mattcowley.co.uk>
2021-10-18 11:51:38 +0100

    Update webpack version and handling in wrangler (#47)
    * Run webpack directly, not using wrangler defaults
    
    * Bump to webpack 5
    
    * Set wrangler compat date

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path');
-const env = require('dotenv').config({ path: path.join(__dirname, `${process.env.NODE_ENV}.env`) });
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const env = require('dotenv').config({ path: path.join(__dirname, `${NODE_ENV}.env`) });
 const { DefinePlugin } = require('webpack');
 const WorkersSentryWebpackPlugin = require('workers-sentry/webpack');
 const build = require('./src/build');
 
+console.log(`Using ${NODE_ENV} environment for build...`);
+
 module.exports = {
+    mode: 'none',
     target: 'webworker',
     entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'worker.js',
+    },
     plugins: [
         // Hook in the commands build process before each webpack run
         { apply: compiler => compiler.hooks.beforeRun.tapPromise('PrepareBuildBeforeWebpack', build) },
@@ -15,7 +23,7 @@ module.exports = {
         new DefinePlugin(Object.entries(env.parsed).reduce((obj, [ key, val ]) => {
             obj[`process.env.${key}`] = JSON.stringify(val);
             return obj;
-        }, { 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) })),
+        }, { 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) })),
 
         // Publish source maps to Sentry on each build
         new WorkersSentryWebpackPlugin(
